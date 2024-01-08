2cf7240ce05912bbb684400bdfce8bd515502e24
Yuriy <kuznecow.yura@gmail.com>
2024-01-08 23:33:35 +0300

    Add overheated cinematic

---
+++ Assets/Scripts/AnimationsScripts/Character/CharacterMoving.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterMoving : MonoBehaviour
+{
+    private float MoveRight = 30f;
+
+    private float MoveLeft = -32f;
+    private float currentMove;
+
+    private float direction;
+
+    public float speed;
+
+    void Start() {
+        currentMove = MoveRight;
+        direction = -1f;
+    }
+
+
+    void Update()
+    {
+
+        if (currentMove == MoveLeft || currentMove == MoveRight) {
+            direction *= -1;
+        transform.Rotate (0, 180, 0);
+        }
+        currentMove = Mathf.Clamp(currentMove + Time.deltaTime * speed * direction, MoveLeft, MoveRight);
+        transform.position = new Vector3(currentMove, 0, 0);
+    }
+}
